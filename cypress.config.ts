9bfc80e11cf79fe6d405e01a512c3d919a428240
Johan Nilsson <yoohahn@gmail.com>
2022-07-01 08:36:05 +0200

    fix(deps): lint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   viewportWidth: 1920,
@@ -11,9 +11,9 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:3433',
-    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
+    baseUrl: "http://localhost:3433",
+    excludeSpecPattern: ["**/__snapshots__/*", "**/__image_snapshots__/*"],
   },
-})
+});
