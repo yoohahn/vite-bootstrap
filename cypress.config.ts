f543c809da736dc264442d3964690d7bc10335b1
Johan Nilsson <yoohahn@gmail.com>
2022-07-01 10:32:18 +0200

    fix(deps): cypress v10 (#151)
    * fix(deps): cypress v10
    
    * fix(deps): lint
    
    * fix(deps): latest cypress in action
    
    * fix(deps): spec path

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  videoUploadOnPasses: false,
+  videoCompression: 15,
+  chromeWebSecurity: false,
+  env: {},
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "http://localhost:3433",
+    excludeSpecPattern: ["**/__snapshots__/*", "**/__image_snapshots__/*"],
+  },
+});
